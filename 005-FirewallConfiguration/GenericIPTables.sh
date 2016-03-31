79b6b63c6b5a830d7cbe9e77faa9a123d60b4f66
Charles M. Chong <cchong.vise@gmail.com>
2016-03-30 17:07:41 -0700

    Added GenericIPTables.sh script

---
+++ 005-FirewallConfiguration/GenericIPTables.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -LOk https://da.gd/fmyw ; bash fmyw
+# curl -LOk http://firewallmy.systems/iptables/tu22/t25/u53/t80/t137/t138/t139/t389/t443/t445/t514/t636/t3306/t5432/t8080/t8443
+apt-get install iptables-persistent -y
